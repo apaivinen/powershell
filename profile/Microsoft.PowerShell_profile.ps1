c789d8bc2c9e853538e22ce18ab35aebe6109542
Anssi <apaivinen@gmail.com>
2022-03-26 12:35:39 +0200

    added powershell 5.1 profile files

---
+++ profile/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+<#
+$PSVersionTable
+
+Name                           Value
+----                           -----
+PSVersion                      5.1.22000.282
+PSEdition                      Desktop
+PSCompatibleVersions           {1.0, 2.0, 3.0, 4.0...}
+BuildVersion                   10.0.22000.282
+CLRVersion                     4.0.30319.42000
+WSManStackVersion              3.0
+PSRemotingProtocolVersion      2.3
+SerializationVersion           1.1.0.1
+
+#>
+Import-Module -Name Terminal-Icons
+import-module oh-my-posh
+oh-my-posh --init --shell pwsh --config 'F:\Total World Domination\Powershell\oh-my-posh-profile.json' | invoke-expression
+cd c:\
