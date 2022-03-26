979b75e023fedbe1eedfed78ff22708dd3b0e276
Anssi <apaivinen@gmail.com>
2022-03-26 13:14:30 +0200

    updated profile file

--- profile/Microsoft.PowerShell_profile.ps1
+++ profile/Microsoft.PowerShell_profile.ps1
@@ -1,19 +1,39 @@
 <#
-$PSVersionTable
+This current profile works with Powershell versions 5.2 and 7.2.2
 
-Name                           Value
-----                           -----
-PSVersion                      5.1.22000.282
-PSEdition                      Desktop
-PSCompatibleVersions           {1.0, 2.0, 3.0, 4.0...}
-BuildVersion                   10.0.22000.282
-CLRVersion                     4.0.30319.42000
-WSManStackVersion              3.0
-PSRemotingProtocolVersion      2.3
-SerializationVersion           1.1.0.1
+*************************
+Module information
+*************************
+
+Terminal icons
+Install-Module -Name Terminal-Icons -Repository PSGallery -Scope CurrentUser
+Currently using 0.5.2
+https://github.com/devblackops/Terminal-Icons
+
+oh-my-posh
+Install-Module oh-my-posh -Repository PSGallery -Scope CurrentUser
+Currently using 7.49.0
+https://ohmyposh.dev/docs/windows#install
+
+PSReadLine
+Install-Module PSReadLine -Repository PSGallery -Scope CurrentUser
+Currently using 2.2.2
+https://github.com/PowerShell/PSReadLine
 
 #>
+
+#import modules
 Import-Module -Name Terminal-Icons
 import-module oh-my-posh
+import-module psreadline
+
+
+#configs
 oh-my-posh --init --shell pwsh --config 'F:\git\powershell\profile\oh-my-posh-profile.json' | invoke-expression
+
+Set-PSReadLineOption -PredictionSource History
+set-PSReadLineOption -PredictionViewStyle ListView
+Set-PSReadLineOption -Colors @{"InlinePrediction"="#478390"}
+Set-PSReadLineOption -HistoryNoDuplicates
+
 cd c:\
