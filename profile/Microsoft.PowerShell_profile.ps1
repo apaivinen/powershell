e241205cd90c39d3e4df060801837a23f9b5a726
Anssi <apaivinen@gmail.com>
2022-03-26 13:21:16 +0200

    moved powershell module informations to readme

--- profile/Microsoft.PowerShell_profile.ps1
+++ profile/Microsoft.PowerShell_profile.ps1
@@ -1,25 +1,5 @@
 <#
 This current profile works with Powershell versions 5.2 and 7.2.2
-
-*************************
-Module information
-*************************
-
-Terminal icons
-Install-Module -Name Terminal-Icons -Repository PSGallery -Scope CurrentUser
-Currently using 0.5.2
-https://github.com/devblackops/Terminal-Icons
-
-oh-my-posh
-Install-Module oh-my-posh -Repository PSGallery -Scope CurrentUser
-Currently using 7.49.0
-https://ohmyposh.dev/docs/windows#install
-
-PSReadLine
-Install-Module PSReadLine -Repository PSGallery -Scope CurrentUser
-Currently using 2.2.2
-https://github.com/PowerShell/PSReadLine
-
 #>
 
 #import modules
